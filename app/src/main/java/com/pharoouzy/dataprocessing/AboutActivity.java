56395b544aaddb193a6266f34c2453b7b903ef2d
Pharoouzy <pharoouzy@yahoo.com>
2021-07-31 17:10:15 +0100

    updates

--- app/src/main/java/com/pharoouzy/dataprocessing/AboutActivity.java
+++ app/src/main/java/com/pharoouzy/dataprocessing/AboutActivity.java
@@ -1,10 +1,13 @@
 package com.pharoouzy.dataprocessing;
 
 import android.os.Build;
-import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class AboutActivity extends AppCompatActivity {
     Toolbar toolbar;
